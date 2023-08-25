da66d09d477575f2fa12d540c0de01e0fe359681
andreas <andreasds@csd.auth.gr>
2023-08-25 16:48:17 +0100

    store weather data to cache

--- animalpedia-app/src/components/WeatherWidget.js
+++ animalpedia-app/src/components/WeatherWidget.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { FaWind, FaTint, FaTemperatureLow  } from 'react-icons/fa';
+import { FaWind, FaTint, FaTemperatureLow } from 'react-icons/fa';
 
 import axiosRequests from '../api/apiCalls'
 
@@ -11,8 +11,8 @@ const WeatherWidget = ({ city }) => {
     useEffect(() => {
         const fetchData = async () => {
             await axiosRequests.getForecast(city)
-                .then(res => {                
-                    setWeather(res.data);                    
+                .then(res => {
+                    setWeather(res.data);
                 });
         };
         fetchData();
@@ -21,44 +21,39 @@ const WeatherWidget = ({ city }) => {
     return (
         <div className="container">
             <div className="row d-flex justify-content-center align-items-center">
-                <div className="">
+                <div className="card" style={{ color: "#4B515D", borderRadius: "35px" }}>
+                    <div className="card-body p-4">
 
-                    <div className="card" style={{ color: "#4B515D", borderRadius: "35px" }}>
-                        <div className="card-body p-4">
+                        <div className="d-flex">
+                            <h6 className="flex-grow-1">{weather.name}</h6>
+                            <h6>{weather.localtime}</h6>
+                        </div>
 
-                            <div className="d-flex">
-                                <h6 className="flex-grow-1">{weather.name}</h6>
-                                <h6>{weather.localtime}</h6>                                
-                            </div>
+                        <div className="d-flex flex-column text-center mt-2 mb-2">
+                            <h6 className="display-4 mb-0 font-weight-bold" style={{ color: "#1C2331" }}>{weather.temp_c}°C </h6>
+                            <span className="small" style={{ color: "#868B94" }}>{weather.text}</span>
+                        </div>
 
-                            <div className="d-flex flex-column text-center mt-2 mb-2">
-                                <h6 className="display-4 mb-0 font-weight-bold" style={{ color: "#1C2331" }}>{weather.temp_c}°C </h6>
-                                <span className="small" style={{ color: "#868B94" }}>{weather.text}</span>
-                            </div>
-
-                            <div className="d-flex align-items-center">
-                                <div className="flex-grow-1" style={{ fontSize: "1rem" }}>
-                                    <div>
-                                        <FaWind /> <span className="ms-1">{weather.wind_kph} km/h</span>
-                                    </div>
-                                    <div>
-                                        <FaTint /> <span className="ms-1">{weather.humidity}%</span>
-                                    </div>
-                                    <div>
-                                        <FaTemperatureLow  /> <span className="ms-1">{weather.feelslike_c}°C</span>
-                                    </div>
+                        <div className="d-flex align-items-center">
+                            <div className="flex-grow-1" style={{ fontSize: "1rem" }}>
+                                <div>
+                                    <FaWind /> <span className="ms-1">{weather.wind_kph} km/h</span>
                                 </div>
                                 <div>
-                                    <img src={`https:${weather.icon}`} alt='weather icon'/>
+                                    <FaTint /> <span className="ms-1">{weather.humidity}%</span>
+                                </div>
+                                <div>
+                                    <FaTemperatureLow /> <span className="ms-1">{weather.feelslike_c}°C</span>
                                 </div>
                             </div>
-
+                            <div>
+                                <img src={`https:${weather.icon}`} alt='weather icon' />
+                            </div>
                         </div>
-                    </div>
 
+                    </div>
                 </div>
             </div>
-
         </div>
 
     )
--- animalpedia-backend/controllers/weather.controller.js
+++ animalpedia-backend/controllers/weather.controller.js
@@ -1,12 +1,35 @@
 const axios = require('axios');
+const Redis = require('redis')
 
+const redisClient = Redis.createClient();
+redisClient.connect();
 async function getForecast(req, res) {
     const city = req.params.city;
-    try {
-        await axios.get(`http://api.weatherapi.com/v1/current.json?key=${process.env.WEATHER_API_KEY}&q=${city}`)
-        .then(response =>{
-           
-            res.status(200).json({
+
+    let redisResult = await redisClient.get('weather')
+    if (redisResult) {
+        console.log("aa")
+        const results = JSON.parse(redisResult);
+        return res.status(200).json(results);
+    } else {
+        try {
+            const response = await axios.get(`http://api.weatherapi.com/v1/current.json?key=${process.env.WEATHER_API_KEY}&q=${city}`)
+
+            const data = {
+                name: response.data.location.name,
+                localtime: response.data.location.localtime,
+                temp_c: response.data.current.temp_c,
+                text: response.data.current.condition.text,
+                wind_kph: response.data.current.wind_kph,
+                humidity: response.data.current.humidity,
+                feelslike_c: response.data.current.feelslike_c,
+                icon: response.data.current.condition.icon
+            }
+             // Send the API response back to the client
+             await redisClient.set('weather', JSON.stringify(data));
+             redisClient.expire('weather', 3600);
+
+             return res.status(200).json({
                 name: response.data.location.name,
                 localtime: response.data.location.localtime,
                 temp_c: response.data.current.temp_c,
@@ -16,10 +39,10 @@ async function getForecast(req, res) {
                 feelslike_c: response.data.current.feelslike_c,
                 icon: response.data.current.condition.icon
             });
-        })
-    } catch (error) {
-        console.error(error);
-        res.status(500).send('Error fetching data from the API.');
+        } catch (error) {
+            console.error(error);
+            res.status(500).send('Error fetching data from the API.');
+        }
     }
 }
 
