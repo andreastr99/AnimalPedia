65771eb25d9e9180bb25433566ed82971fdcc30b
andreas <andreasds@csd.auth.gr>
2023-08-31 13:32:28 +0100

    fill base | show based on the provided parameter

--- animalpedia-backend/controllers/animal.controller.js
+++ animalpedia-backend/controllers/animal.controller.js
@@ -3,7 +3,7 @@ const AnimalModel = require('../models/animalModel')
 
 function getAllAnimals(req, res) {
     AnimalModel.find().then(response => {
-        res.status(200).json(response[0])
+        res.status(200).json(response)
     })
         .catch(error => {
             res.status(500).json({
@@ -12,33 +12,60 @@ function getAllAnimals(req, res) {
         })
 }
 
+function show(req, res) {
+    // Define a query object to filter by continent if provided
+    const showBy = req.params.showBy;
+
+    const query = {};
+
+    // Add filters based on the provided parameter
+    if (showBy) {
+        query.$or = [
+            { "name.common": showBy },
+            { "class": showBy },
+            { "continent": showBy }
+        ];
+    }
+
+    AnimalModel.find(query).then(response => {
+        res.status(200).json(response)
+    })
+        .catch(error => {
+            res.status(500).json({
+                message: "Error retrieving data!"
+            })
+        })
+}
+
+
 function addAnimal(req, res) {
     const { name, classs, continent, details, youtbeLink, favourite, image } = req.body;
 
     let animal = new AnimalModel({
         name: name,
-        classs: classs, 
-        continent: continent,  
-        details: details, 
-        youtbeLink: youtbeLink, 
+        classs: classs,
+        continent: continent,
+        details: details,
+        youtbeLink: youtbeLink,
         favourite: favourite,
         image: image
     })
 
-    animal.save().then(response =>{
+    animal.save().then(response => {
         res.status(200).json({
             message: "Animal added successfully!"
         })
     })
-    .catch(error => {
-        console.error(error)
-        res.status(500).json({
-            message: "An error occured!"
+        .catch(error => {
+            console.error(error)
+            res.status(500).json({
+                message: "An error occured!"
+            })
         })
-    })
 }
 
 module.exports = {
     getAllAnimals: getAllAnimals,
+    show: show,
     addAnimal: addAnimal,
 }
