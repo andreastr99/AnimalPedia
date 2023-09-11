ebedde9debce1c4018bcc4870afd48ddcba9e367
andreas <andreasds@csd.auth.gr>
2023-09-11 16:11:21 +0100

    layout

--- animalpedia-app/src/components/PreviewCard.js
+++ animalpedia-app/src/components/PreviewCard.js
@@ -56,43 +56,51 @@ const PreviewCard = ({ animalName = null, favouriteAnimal = null }) => {
     }, [favouriteAnimal]);
     return (
         <div className="col-md-4">
-            <div className="card flex-md-row mb-4 bshadow-sm h-md-250" style={{ background: "#f2f1eb" }}>
+            <div className="card mb-4 shadow-sm" style={{ background: "#f2f1eb" }}>
                 {loading ? (
                     <div className="card-body d-flex flex-column align-items-start" >
                         loading...
                     </div>
                 ) : (
                     <>
-                        <div className="card-body d-flex flex-column align-items-start">
-                            <strong className="d-inline-block mb-2" style={{ color: "#00704A" }}>{animal.class}</strong>
-                            <h4 className="mb-0">
-                                <u className="text-dark">{animalName ? animal.name[0] : animal.name.common}</u>
-                            </h4>
-                            <div className="text-muted">{animalName ? animal.order : ""}</div>
-                            <p className="card-text mb-auto"><strong>Can be found in: </strong>
-                                {animalName ? (
-                                    animal.continent.map((continent, index) => (
-                                        <span key={index}><em> {continent}</em></span>
-                                    ))
-                                ) : (
-                                    animal.continent
-                                )}
+                        <div className='container pr-1 pt-1'>
+                            <div className="row " style={{ height: "150px" }}>
+                                <div className='col mt-1'>
+                                    <strong className="d-inline-block mb-1" style={{ color: "#00704A" }}>{animal.class}</strong>
+                                    <h4 className="mb-0">
+                                        <u className="text-dark">{animalName ? animal.name[0] : animal.name.common}</u>
+                                    </h4>
+                                    <div className="text-muted">{animalName ? animal.order : "order"}</div>
+                                    <p className="card-text mb-auto"><strong>Can be found in: </strong>
+                                        {animalName ? (
+                                            animal.continent.map((continent, index) => (
+                                                <span key={index}><em> {continent}</em></span>
+                                            ))
+                                        ) : (
+                                            animal.continent
+                                        )}
+                                    </p>
+                                </div>
+                                <div className='col'>
+                                    <img className='img-thumbnail float-right p-0' src={animalName ? link : animal.image} alt={animalName ? animal.name : animal.name.common} style={animalName ? { maxHeight: "180px" } : { maxHeight: "150px" }} />
+                                </div>
+                            </div>
 
-                            </p>
-                            {animalName ? (
-                                <>
-                                    <button onClick={handleShowModal} className="btn btn-success btn-sm mt-auto">View Animal Card</button>
-                                    <AnimalCardModal showModal={showModal} handleClose={handleCloseModal} animalImage={`${link}`} animal={animal} />
-                                </>
-                            ) : (
-                                <button className="btn btn-success btn-sm mt-auto">
-                                    <Link to={`/animal/${animal._id}`} style={{ textDecoration: "none", color: "white" }}> View Animal Card</Link>
-                                </button>
-                            )}
-
-
+                            <div className='row'>
+                                <div className='col align-self-end mb-2'>
+                                    {animalName ? (
+                                        <>
+                                            <button onClick={handleShowModal} className="btn btn-success btn-sm mt-auto">View Animal Card</button>
+                                            <AnimalCardModal showModal={showModal} handleClose={handleCloseModal} animalImage={`${link}`} animal={animal} />
+                                        </>
+                                    ) : (
+                                        <button className="btn btn-success btn-sm mt-auto">
+                                            <Link to={`/animal/${animal._id}`} style={{ textDecoration: "none", color: "white" }}> View Animal Card</Link>
+                                        </button>
+                                    )}
+                                </div>
+                            </div>
                         </div>
-                        <img className='img-thumbnail ' src={animalName ? link : animal.image} alt={animalName ? animal.name : animal.name.common} style={animalName ? { maxHeight: "180px" } : { maxHeight: "150px" }} />
                     </>
                 )}
             </div>
